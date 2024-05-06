65789b284a5af686bac706d41386e575bb3f8aa5
Aidan LaBella <apl1341@rit.edu>
2024-05-06 14:51:09 -0400

    update report and models

--- src/python/plot_evaluations.py
+++ src/python/plot_evaluations.py
@@ -4,7 +4,7 @@ import sys
 
 df = pd.read_csv(sys.argv[1])
 
-cols = ['predicted_SO4', 'predicted_SO2', 'predicted_O3', 'predicted_CO']
+cols = ['predicted_SO4', 'predicted_SO2', 'predicted_O3', 'predicted_CO', 'expected_SO4', 'expected_SO2', 'expected_O3', 'expected_CO']
 
 fig = px.line(df, x=df.index, y=cols, title='Predicted vs. Actual')
 fig.update_layout(xaxis_title='Timestep', yaxis_title='Values')
